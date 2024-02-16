335611e388b7922fd397acebe6533ebeb535da5d
kianm <keyanm@gmail.com>
2024-02-16 11:42:13 +0100

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "MotionTagSDK",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "MotionTagSDK",
+            targets: ["MotionTagSDK"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "MotionTagSDK",
+            url: "https://github.com/MOTIONTAG/motiontag-sdk-ios-releases/archive.zip",
+            checksum: "12345"
+        )
+    ]
+)
