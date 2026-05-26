285dfebffe3830c1f801dab7f6a4a09f96a9df9b
kianm <keyanm@gmail.com>
2026-05-26 07:57:09 +0200

    7.0.0-internal - commit: e460e305

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "MotionTagSDK",
-            url: "https://mtbetatesting.web.app/archive_6.5.0_2026020601.zip",
-            checksum: "5be5c5445fafe2b3bf4641d201b48f635cde801cd424791ff09e06a0524a0893"
+            url: "https://mtbetatesting.web.app/archive_7.0.0_2026052601.zip",
+            checksum: "0d4ae2a5747a5012a6ed48cabecbd14b135523a1c3c9d15f99b2751966960267"
         )
     ]
 )
